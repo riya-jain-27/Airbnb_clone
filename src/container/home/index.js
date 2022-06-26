225f4c5c09f44c25fedfe75047da1b7835a0c659
Riya Jain <riyajain27riya@gmail.com>
2022-06-26 22:27:22 +0530

    auth correction

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -11,21 +11,21 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 function Home() {
     const navigate = useNavigate();
 
-    useEffect(()=>{
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            const uid = user.uid;
-            // ...
-        } else {
-            // User is signed out
-            // ...
-            navigate("/");
-        }
-        });
-    },[])
+    // useEffect(()=>{
+    //     const auth = getAuth();
+    //     onAuthStateChanged(auth, (user) => {
+    //     if (user) {
+    //         // User is signed in, see docs for a list of available properties
+    //         // https://firebase.google.com/docs/reference/js/firebase.User
+    //         const uid = user.uid;
+    //         // ...
+    //     } else {
+    //         // User is signed out
+    //         // ...
+    //         navigate("/");
+    //     }
+    //     });
+    // },[])
 
     return(
         <>
